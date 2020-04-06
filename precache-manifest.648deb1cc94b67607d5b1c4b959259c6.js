b6386e7362089f17073cc60cdb44ba04ebb5e808
Danielius Korsakas <danielius.korsakas@gmail.com>
2020-04-06 14:51:34 +0300

    Updates

---
+++ precache-manifest.648deb1cc94b67607d5b1c4b959259c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda136ca8033eaaadfeb107012cb4101",
+    "url": "/project-rolodex.git/index.html"
+  },
+  {
+    "revision": "894fb3eafdb8b31904e7",
+    "url": "/project-rolodex.git/static/css/main.4d010ddb.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/project-rolodex.git/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-rolodex.git/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "894fb3eafdb8b31904e7",
+    "url": "/project-rolodex.git/static/js/main.e8c2ad05.chunk.js"
+  },
+  {
+    "revision": "40e29058026c7efa4adb",
+    "url": "/project-rolodex.git/static/js/runtime-main.c298e5e4.js"
+  }
+]);
